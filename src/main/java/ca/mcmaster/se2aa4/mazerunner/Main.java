540723a77e9f91e4fe0c89860ffacaa5edacc770
Suzanne <abduls61@mcmaster.ca>
2025-01-22 20:26:17 -0500

    STEP 2: introduced function definitions and walking skeleton in main, commented out template code

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,13 +11,28 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLineParser;
 
 public class Main {
+    String maze;
 
     private static final Logger logger = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    private void fileProcessor(){ //reads file and assigns to maze variable
+
+    }
+
+    private String pathFinder(){ //processes string and returns its canonical path
+        return "placeholder";
+    }
+
+    private boolean pathVerify(String path){ //processes the maze with given path and returns a boolean representing whether or not path is valid
+        return true;
+    }
+
+    public static void main(String[] args) { //process args and provide UI statements
         Options options = new Options();
         options.addOption("i", true,"Traverse the maze");
+        options.addOption("p", true,"Verify a path through a maze is valid");
         CommandLineParser parser = new DefaultParser();
+        Main m = new Main();
 
         logger.info("** Starting Maze Runner");
         //System.out.println("** Starting Maze Runner"); //info
@@ -27,9 +42,19 @@ public class Main {
 
             if (cmd.hasOption("i")){
                 logger.info("**** Reading the maze from file ", args[1]);
+                m.fileProcessor();
+
+                if (cmd.hasOption("p")){
+                    boolean valid = m.pathVerify(args[3]);
+                }
+                    
+                else {
+                    String path = m.pathFinder();
+                }
+
                 //System.out.println("**** Reading the maze from file " + args[1]); //info
 
-                BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+                /**BufferedReader reader = new BufferedReader(new FileReader(args[1]));
                 String line;
                 while ((line = reader.readLine()) != null) {
                     for (int idx = 0; idx < line.length(); idx++) {
@@ -41,7 +66,9 @@ public class Main {
                     }
                     System.out.print(System.lineSeparator());
                 }
+                **/
             }
+
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
             //System.err.println("/!\\ An error has occured /!\\"); //error
