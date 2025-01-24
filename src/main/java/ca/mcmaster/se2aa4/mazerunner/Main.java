86ef1d76c768ba8789953ba6217cb25cfd7af9dc
Suzanne <abduls61@mcmaster.ca>
2025-01-24 18:23:29 -0500

    closes #4; STEP 4: added pathfinder functionality and added more descipritive logger messages

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,6 +1,7 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
@@ -11,19 +12,91 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLineParser;
 
 public class Main {
-    String maze;
+    ArrayList<String> maze = new ArrayList<String>();
 
     private static final Logger logger = LogManager.getLogger();
 
-    private void fileProcessor(){ //reads file and assigns to maze variable
+    private void fileProcessor(String filename){ //reads file and assigns to maze variable 
+        String line;
+        
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
 
+            while ((line = reader.readLine()) != null) {
+                maze.add(line);
+            }  
+            
+        } catch(Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
+            System.out.println("PATH NOT COMPUTED"); //info
+            //System.err.println("/!\\ An error has occured /!\\"); //error
+        }
     }
 
     private String pathFinder(){ //processes string and returns its canonical path
-        return "placeholder";
+        int lines = maze.size();
+        int len = 0;
+        int current_line = 0;
+        String path = "";
+        String item = "";
+
+        for (int i=0; i<lines; i++){
+            item = maze.get(i);
+            len = item.length();
+
+            if (item.charAt(0) == '#'){
+                logger.info("**** no entrance, pass");
+            }
+
+            else{
+                logger.info("**** entrance found!");
+                current_line = i;
+                i=lines;
+            }
+        }
+        
+        for (int j=0; j<len; j++){
+            item = maze.get(current_line);
+            if (item.charAt(j) == ' '){
+                path = path + "F";
+            }
+            //add else statement accounting for turns 
+        }
+        
+        return path;
+
     }
 
     private boolean pathVerify(String path){ //processes the maze with given path and returns a boolean representing whether or not path is valid
+        /**int lines = maze.size();
+        String path = "";
+        int current_line = 0;
+        boolean valid_path = false;
+    
+        for (int i=0; i<lines; i++){
+            String item = maze.get(i);
+            int len = item.length();
+
+            if (item.charAt(0) == '#'){
+                logger.info("**** no entrance, pass");
+            }
+
+            else{
+                logger.info("**** entrance found!");
+                current_line = i;
+                i=lines;
+            }
+
+        }
+        
+        for (int j=0; j < path.length(); j++){ //traversing 
+            item = maze.get(current_line);
+            if (path.charAt(j) == 'F'){
+                current_line;    
+            }
+        }
+
+        return valid_path;**/
         return true;
     }
 
@@ -41,20 +114,23 @@ public class Main {
             CommandLine cmd = parser.parse(options, args);
 
             if (cmd.hasOption("i")){
-                logger.info("**** Reading the maze from file ", args[1]);
-                m.fileProcessor();
+                logger.info("**** Reading the maze from file", args[1]);
+                m.fileProcessor(args[1]);
 
                 if (cmd.hasOption("p")){
                     boolean valid = m.pathVerify(args[3]);
                 }
                     
                 else {
+                    logger.info("**** Computing path"); //info
                     String path = m.pathFinder();
+                    System.out.println(path);
                 }
 
                 //System.out.println("**** Reading the maze from file " + args[1]); //info
-
-                /**BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+                
+                /**
+                BufferedReader reader = new BufferedReader(new FileReader(args[1]));
                 String line;
                 while ((line = reader.readLine()) != null) {
                     for (int idx = 0; idx < line.length(); idx++) {
@@ -67,14 +143,15 @@ public class Main {
                     System.out.print(System.lineSeparator());
                 }
                 **/
+                
             }
 
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
+            System.out.println("PATH NOT COMPUTED"); //info
             //System.err.println("/!\\ An error has occured /!\\"); //error
         }
-        logger.info("**** Computing path"); //info
-        System.out.println("PATH NOT COMPUTED"); //info
+
         logger.info("** End of MazeRunner"); //info
     }
 }
